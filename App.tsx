74a5fa80ae6937232aca69ce7e7ba349f840f58e
Keisuke Kakuda <k.keisuke.1230@gmai.com>
2021-08-15 17:37:29 +0900

    first commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+if (!firebase.apps.length) {
+  const firebaseConfig = {
+    apiKey: "AIzaSyCL6QxT66Wk5zTnhLfpKhaLuDp9eKBrlTA",
+    authDomain: "shop-review-34578.firebaseapp.com",
+    projectId: "shop-review-34578",
+    storageBucket: "shop-review-34578.appspot.com",
+    messagingSenderId: "704873982351",
+    appId: "1:704873982351:web:e83af5528b70ac08ffc3fa",
+    measurementId: "G-M9ZHRTHG7X",
+  };
+  firebase.initializeApp(firebaseConfig);
+}
+
+type Shop = {
+  name: string;
+  place: string;
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [shops, setShops] = useState<Shop[]>([]);
+  useEffect(() => {
+    getFirebaseItems();
+  }, []);
+
+  const getFirebaseItems = async () => {
+    const snapshot = await firebase.firestore().collection("shops").get();
+    const shops = snapshot.docs.map((doc) => doc.data() as Shop);
+    setShops(shops);
+    console.log(shops);
+  };
+
+  const shopItems = shops.map((shop, index) => {
+    return (
+      <View style={{ margin: 10 }} key={index.toString()}>
+        <Text>{shop.name}</Text>
+        <Text>{shop.place}</Text>
+      </View>
+    );
+  });
+
+  return <View style={styles.container}>{shopItems}</View>;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
